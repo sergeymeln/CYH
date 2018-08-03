f21f7555b3a95cf8435a440b1775945e88179e3d
SergeyM <sergey.melnichenko@antarasoft.com>
2018-08-03 13:01:33 +0300

    Statistic collection

---
+++ plugins/sal-core-marketing/src/internal/CYH/Marketing/Services/StatisticsService.php
@@ -0,0 +1,75 @@
+<?php
+
+
+namespace CYH\Marketing\Services;
+
+use CYH\Marketing\Repositories\StatisticsRepository;
+use CYH\Sal\Services\Base\CacheableService;
+
+class StatisticsService
+{
+    private static $instance = null;
+    protected static $statisticsRepository = null;
+    const EVENT_TYPES = [
+        'page_generation_start' => 1,
+        'request_to_sal_start' => 2,
+        'request_to_sal_stop' => 3,
+        'data_preparation_complete' => 4,
+        'view_rendering_complete' => 5
+    ];
+    protected $eventObjects = [];
+
+
+    public static function getInstance()
+    {
+        if (null === self::$instance)
+        {
+            self::$instance = new self();
+            self::$statisticsRepository = new StatisticsRepository();
+        }
+        return self::$instance;
+    }
+    private function __clone() {}
+    private function __construct() {}
+
+    /**
+     * @param $type
+     * @param $time
+     */
+    public function addObject($type, $time)
+    {
+        $eventObject = $this->createObject($type, $time);
+        array_push($this->eventObjects, $eventObject);
+    }
+
+    /**
+     * @param $type
+     * @param $time
+     * @return \stdClass
+     */
+    private function createObject($type, $time)
+    {
+        $eventObject = new \stdClass();
+        $eventObject->event_type = $type;
+        $eventObject->actual_timestamp = $time;
+
+        return $eventObject;
+    }
+
+    /**
+     * @return mixed
+     */
+    private function generateRequestId()
+    {
+        return microtime(true);
+    }
+
+    public function insertStatistics()
+    {
+        $requestId = $this->generateRequestId();
+        foreach ($this->eventObjects as $stat) {
+            self::$statisticsRepository->insertStatistics($stat, $requestId);
+        }
+    }
+
+}
--- themes/connect-your-home/page-templates/marketing.php
+++ themes/connect-your-home/page-templates/marketing.php
@@ -6,11 +6,22 @@
  * @subpackage cyh
  *
  */
+use CYH\Marketing\Services\StatisticsService;
+use CYH\WpOptionsHandlers\Pages\GeneralOptions;
+$generalOptions = GeneralOptions::GetSettings();
+
+$collectStats = false;
+if (isset($generalOptions['sal_enable_statistic']) && $generalOptions['sal_enable_statistic'] == 'on') {
+    $collectStats = true;
+    $statService = StatisticsService::getInstance();
+    $statService->addObject(1, microtime(true));
+}
+
 
 get_header('sal');
 
 try{
-    do_action('\\' . CYH\Marketing\Controllers\ConnectYourHome\MarketingsController::class . '::RenderMarketing');
+    do_action('\\' . CYH\Marketing\Controllers\ConnectYourHome\MarketingsController::class . '::RenderMarketing', $collectStats);
 }
 catch (Exception $ex)
 {
@@ -18,3 +29,8 @@ catch (Exception $ex)
     wp_redirect($settings['general_error_page']);
 }
 get_footer('sal');
+
+if (isset($generalOptions['sal_enable_statistic']) && $generalOptions['sal_enable_statistic'] == 'on') {
+    $statService->addObject(5, microtime(true));
+    $statService->insertStatistics();
+}
